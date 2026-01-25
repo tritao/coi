3f828ff60d68d65a5fbc4161c0467c03c33e7938
tritao <joao@tritao.eu>
2026-01-25 11:48:39 +0000

    visual: add paint_image_clip_radius_edges scene

--- tests/run_visual_desktop.sh
+++ tests/run_visual_desktop.sh
@@ -105,6 +105,7 @@ scenes=(
   "paint_border_radius_combo:$ROOT_DIR/tests/desktop/visual/scenes/paint/border_radius_combo.coi"
   "paint_floating_zindex:$ROOT_DIR/tests/desktop/visual/scenes/paint/floating_zindex.coi"
   "paint_image_basic:$ROOT_DIR/tests/desktop/visual/scenes/paint/image_basic.coi"
+  "paint_image_clip_radius_edges:$ROOT_DIR/tests/desktop/visual/scenes/paint/image_clip_radius_edges.coi"
   "layout_row_fixed_and_grow:$ROOT_DIR/tests/desktop/visual/scenes/layout/row_fixed_and_grow.coi"
   "layout_col_fixed_and_grow:$ROOT_DIR/tests/desktop/visual/scenes/layout/col_fixed_and_grow.coi"
   "layout_nested_flex:$ROOT_DIR/tests/desktop/visual/scenes/layout/nested_flex.coi"
